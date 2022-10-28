d0ab969a4c068d4d80bfac5552d24cd2e3715758
xNans <95973544+xNans@users.noreply.github.com>
2022-10-28 10:55:47 +0200

    Jobs Done
    Min hamburger menu er ikke funktionel, brug venligst browser menu

---
+++ storefront-child/page-forside.php
@@ -0,0 +1,24 @@
+<?php get_header()?>
+<?php while(have_posts()): the_post() ?>
+<section class="forside1">¨
+    <div class="forsideheader">
+        <h1 class="forsideh1">Woo Blomster</h1>
+    </div>
+    <div class="overall-forside">
+        <div class="forsidecontent">
+            <div class="forsidecontent1">
+                <p class="produkt1">Blomster til både inde og ude</p>
+            </div>
+            <div class="forsidecontent2">
+                <p class="produkt2">Planter til både inde og ude</p>
+            </div>
+            
+            <div class="forsidecontent3">
+                    <p class="produkt3">Smukke og eksotiske træer</p>
+                </div>
+
+        </div>
+        <a href="http://nmcdesign.dk/shop/" class="shopbtn">Shoppen</a>
+    </div>
+</section>
+<?php endwhile ?>
